26d0177aaa9a0018a95c4dc20a2a1518091c6923
Adi Fatkhurozi <adifatkhurozi@192.168.1.8>
2023-06-06 21:19:39 +0700

    ci: update ci

---
+++ dist/test/index.test.js
@@ -0,0 +1,8 @@
+"use strict";
+// test/index.test.ts
+describe('Example', () => {
+    it('should pass', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
+//# sourceMappingURL=index.test.js.map
