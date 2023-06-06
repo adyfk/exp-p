9c66e255bdf2c6a1daf19634892e0769c4493ea0
Adi Fatkhurozi <adifatkhurozi@192.168.1.8>
2023-06-06 21:28:42 +0700

    fix: update ci deployment

--- dist/test/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// test/index.test.ts
-describe('Example', () => {
-    it('should pass', () => {
-        expect(1 + 1).toBe(2);
-    });
-});
-//# sourceMappingURL=index.test.js.map
