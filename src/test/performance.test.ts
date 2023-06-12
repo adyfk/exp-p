263fed96e0ecc7dd4b7b2272c003bc82190c4521
Adi Fatkhurozi <adifatkhurozi@192.168.1.8>
2023-06-13 02:47:16 +0700

    fix: update performance test

--- src/test/performance.test.ts
+++ src/test/performance.test.ts
@@ -1,34 +1,34 @@
 
-// import Benchmark from "benchmark";
-// import { init, formula } from "expressionparser";
-// import ExpressionParser from "..";
+import Benchmark from "benchmark";
+import { init, formula } from "expressionparser";
+import { createParser } from "..";
 
-// test('test', () => {
-//   const p2 = init(formula, (): any => {
-//     return null;
-//   });
+test('test', () => {
+  const p2 = init(formula, (): any => {
+    return null;
+  });
 
-//   const p = new ExpressionParser();
+  const p = createParser();
 
-//   const suite = new Benchmark.Suite();
+  const suite = new Benchmark.Suite();
 
-//   suite.add("exp-p", () => {
-//     p.evaluate("(2 + 3) * 4 - 4");
-//   });
+  suite.add("expressionparser", () => {
+    p2.expressionToValue("[1,2,3,4,5,6,7,1,3,5, 5 + 5 + 5 + ( 2 * 4 + 2)]");
+  });
+  suite.add("exp-p", () => {
+    p.evaluate("[1,2,3,4,5,6,7,1,3,5, 5 + 5 + 5 + ( 2 * 4 + 2)]");
+  });
 
-//   suite.add("expressionparser", () => {
-//     p2.expressionToValue("(2 + 3) * 4 - 4");
-//   });
 
-//   suite.on('cycle', function (event: any) {
-//     console.log(String(event.target));
-//   })
+  suite.on('cycle', function (event: any) {
+    console.log(String(event.target));
+  })
 
-//   suite.on('complete', function () {
-//     console.log('Fastest is ' + this.filter('fastest').map('name'));
-//   })
+  suite.on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
 
-//   suite.run();
-// })
+  suite.run();
+})
 
 test("x", () => { })
